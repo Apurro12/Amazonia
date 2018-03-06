5e51261260f6564664c3ea7360b71b84a3310fb9
Charly <charlymedrano7@gmail.com>
2018-03-06 16:36:21 -0300

    main commit inicial, inicia territorio sin obejtos animales

---
+++ main.py
@@ -0,0 +1,30 @@
+
+# coding: utf-8
+
+# In[1]:
+
+import Clases_animales as animales
+import clase_territorio as territorio
+
+
+# In[2]:
+
+tamaño = [20,20]
+lista_pre = ['p1','p2','p3']
+lista_dep = ['d1','d2','d3']
+
+
+# In[3]:
+
+a = territorio.Territorio(tamaño,lista_pre,lista_dep)
+
+
+# In[4]:
+
+a.iniciar()
+
+
+# In[ ]:
+
+
+
